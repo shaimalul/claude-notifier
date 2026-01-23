e912e5f8432a280ca9bb3af65f72244e25546525
Shai <shaimalul89@gmail.com>
2026-01-23 09:10:27 +0200

    test: add unit tests for models and services

---
+++ Tests/ClaudeNotifierTests/Config/AppConfigTests.swift
@@ -0,0 +1,33 @@
+import XCTest
+@testable import ClaudeNotifier
+
+final class AppConfigTests: XCTestCase {
+    func test_httpPort_hasExpectedValue() {
+        XCTAssertEqual(AppConfig.httpPort, 19847)
+    }
+
+    func test_logFilePath_isNotEmpty() {
+        XCTAssertFalse(AppConfig.logFilePath.isEmpty)
+        XCTAssertTrue(AppConfig.logFilePath.hasSuffix(".log"))
+    }
+
+    func test_soundFilePath_pointsToSystemSound() {
+        XCTAssertTrue(AppConfig.soundFilePath.contains("/System/Library/Sounds/"))
+    }
+
+    func test_notificationCategoryIdentifier_isNotEmpty() {
+        XCTAssertFalse(AppConfig.notificationCategoryIdentifier.isEmpty)
+    }
+
+    func test_showActionIdentifier_isNotEmpty() {
+        XCTAssertFalse(AppConfig.showActionIdentifier.isEmpty)
+    }
+
+    func test_cursorAppBundleIdentifier_isNotEmpty() {
+        XCTAssertFalse(AppConfig.CursorApp.bundleIdentifier.isEmpty)
+    }
+
+    func test_cursorAppName_isCursor() {
+        XCTAssertEqual(AppConfig.CursorApp.appName, "Cursor")
+    }
+}
---
+++ Tests/ClaudeNotifierTests/Mocks/MockResponseBuilder.swift
@@ -0,0 +1,17 @@
+import Foundation
+@testable import ClaudeNotifier
+
+final class MockResponseBuilder: ResponseBuilderProtocol {
+    var buildCalls: [(statusCode: Int, body: String)] = []
+    var mockResponse: Data?
+
+    func build(statusCode: Int, body: String) -> Data? {
+        buildCalls.append((statusCode, body))
+        return mockResponse ?? body.data(using: .utf8)
+    }
+
+    func reset() {
+        buildCalls.removeAll()
+        mockResponse = nil
+    }
+}
